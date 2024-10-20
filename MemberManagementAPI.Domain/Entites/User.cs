1e31845872b17b20a883b80caa0045591872ca4a
Deepesh Kumar <deepesh.kumar@interbizconsulting.com>
2024-10-20 22:19:40 +0530

    Update User.cs

--- MemberManagementAPI.Domain/Entites/User.cs
+++ MemberManagementAPI.Domain/Entites/User.cs
@@ -8,5 +8,16 @@ namespace MemberManagementAPI.Domain.Entites
 {
     internal class User
     {
+        public guid Id{get;set;}
+        [Requied] 
+        public string FirstName{get;set;}
+        [Required]
+        public string LastName{get;set}
+        [Required]
+        public int PhoneNo{get;set;}
+        public string temp{get;set;}
+        public string temp{get;set;}
+        public string temp{get;set;}
+        public string temp{get;set;}
     }
 }
